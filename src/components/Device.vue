ad9ce36fb6e2e28ae9acb0e7907779e7d806b461
inisire <inisire@gmail.com>
2023-12-02 14:48:26 +0200

    favicon, fix popup width on phone

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -28,8 +28,8 @@
 
       <div @click="extended = false;" class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-      <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-        <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg p-4">
+      <div class="flex min-h-full items-start justify-center p-4 text-center">
+        <div style="width: 100%; max-width: 500px;" class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all p-4">
           <slot name="extended"></slot>
         </div>
       </div>
